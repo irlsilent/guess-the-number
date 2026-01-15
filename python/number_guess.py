a772356450f1ecf77615020d7f4f76e65d0a9ee8
Thomas Loonis <117849378+irlsilent@users.noreply.github.com>
2026-01-15 10:37:14 +0000

    Improve Python input validation and edge case handling.

--- python/number_guess.py
+++ python/number_guess.py
@@ -5,6 +5,11 @@ print("Hi! Welcome to the Number Guessing Game\nYou have 7 chances to guess the 
 low = int(input("Enter the lower bound of the range: "))
 high = int(input("Enter the upper bound of the range: "))
 
+while low >= high:
+    print("Your lower bound is equal or higher than the upper bound. Please change your lower bound.")
+    low = int(input("Enter the lower bound of the range: "))
+    high = int(input("Enter the upper bound of the range: "))
+    
 print(f"\nYou have 7 chance to guess a number between {low} and {high}. Let's get started!")
 
 random_number = random.randint(low, high)
@@ -12,7 +17,15 @@ chances = 7
 guess_counter = 0
 
 while guess_counter < chances:
-    user_guess = int(input("Enter your guess: "))
+    while True:
+        try:
+            user_guess = int(input("Enter your guess: "))
+            while user_guess > high:
+                print("Your guess is out of bounds.")
+                user_guess = int(input("Enter your guess: "))
+            break
+        except ValueError:
+            print("That is not a number. Try again!")  
     guess_counter += 1
     
     if (user_guess < random_number):
@@ -27,3 +40,4 @@ if (guess_counter == chances):
     {
         print(f"Oh no! You have used all of your {chances} chances. Too bad!")
     }
+
