1ecb90199d505bf5cb1635215e126a113114c62e
Thomas Loonis <117849378+irlsilent@users.noreply.github.com>
2026-01-15 10:58:18 +0000

    Cleaned up Python code with comments and removed some errors.

--- python/number_guess.py
+++ python/number_guess.py
@@ -1,10 +1,13 @@
 import random
 
+
 print("Hi! Welcome to the Number Guessing Game\nYou have 7 chances to guess the correct number. Let's start!")
 
+#Takes in input from the user and transforms them into Integers.
 low = int(input("Enter the lower bound of the range: "))
 high = int(input("Enter the upper bound of the range: "))
 
+#Checks if the Lower bound is higher or equal to the Upper bound
 while low >= high:
     print("Your lower bound is equal or higher than the upper bound. Please change your lower bound.")
     low = int(input("Enter the lower bound of the range: "))
@@ -12,20 +15,25 @@ while low >= high:
     
 print(f"\nYou have 7 chance to guess a number between {low} and {high}. Let's get started!")
 
+
 random_number = random.randint(low, high)
 chances = 7
+#Limits the amount of chances for the user. Can change later to scale with range size.
 guess_counter = 0
 
+#Main loop logic. Will loop until the guess counter is equal to chances or if the right number was found.
 while guess_counter < chances:
+    #Check to see if User input was a number and inside the range of the bounds.
     while True:
         try:
             user_guess = int(input("Enter your guess: "))
-            while user_guess > high:
+            while user_guess > high or user_guess < low:
                 print("Your guess is out of bounds.")
                 user_guess = int(input("Enter your guess: "))
             break
         except ValueError:
             print("That is not a number. Try again!")  
+            
     guess_counter += 1
     
     if (user_guess < random_number):
@@ -37,7 +45,5 @@ while guess_counter < chances:
             break
         
 if (guess_counter == chances):
-    {
         print(f"Oh no! You have used all of your {chances} chances. Too bad!")
-    }
 
